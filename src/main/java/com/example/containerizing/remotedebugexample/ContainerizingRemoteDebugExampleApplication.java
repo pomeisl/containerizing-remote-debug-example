31442825d29e0f68e9261cad12edfb0114a13b0f
Ferenc Pomeisl <ferencpomeisl@gmail.com>
2020-02-17 21:51:28 +0100

    creates a spring boot web application with spring initializr

---
+++ src/main/java/com/example/containerizing/remotedebugexample/ContainerizingRemoteDebugExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.containerizing.remotedebugexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizingRemoteDebugExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerizingRemoteDebugExampleApplication.class, args);
+	}
+
+}
